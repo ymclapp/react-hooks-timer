3ec41294a9088737fa81d600128ebd0a9f532526
ymcla <ymclapp@yahoo.com>
2021-12-28 15:10:23 -0600

    formatted time

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
     const difference = +new Date(`10/01/${ year }`) - +new Date();
     //To keep the code reusable, you use a JavaScript Template Literal and add in the year variable along with the month and day of Hacktoberfest. Hacktoberfest starts on October 1st each year. When you use the year variable in place of a hard-coded year, you will always have the current year.
     //Now that you calculated the total number of milliseconds until the countdown timer expires, you need to convert the number of milliseconds to something more friendly and human-readable
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days:  Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours:  Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes:  Math.floor((difference / 1000 / 60) % 60),
+        seconds:  Math.floor((difference / 1000) % 60)
+      };
+    }
   }
 
 
