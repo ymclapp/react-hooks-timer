88fe11e11091011d2bda6d40b27e58f4540376c6
ymcla <ymclapp@yahoo.com>
2021-12-28 15:20:27 -0600

    created the setEffect with the setTimeout and clearTimeout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';  //A side effect is anything that affects something outside the scope of the function being executed
 import './App.css';
 
 function App() {
@@ -22,6 +22,22 @@ function App() {
     }
     return timeLeft;  //Finally, you need to return timeLeft so that you can use the value elsewhere in the component
   }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  //The useEffect is what updates the amount of time remaining. By default, React will re-invoke the effect after every render
+  //Every time the variable timeLeft is updated in the state, the useEffect fires. Every time that fires, we set a timer for 1 second (or 1,000ms), which will update the time left after that time has elapsed.
+
+//The cycle will continue every second after that.
+
+//To help to eliminate the potential of stacking timeouts and causing an error, add the clearTimeout method inside the useEffect hook as well
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
 
 
   return (
